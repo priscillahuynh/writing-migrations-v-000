374c7860dace8a9b8b8006a191f53ba1f0818f95
Priscilla H <priscillamhuynh@gmail.com>
2019-01-03 13:40:33 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,7 +1,7 @@
-class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
+class ChangeDataTypeForBirthdate < ActiveRecord::Migration[5.1]
 
   def change
       change_column :students, :birthdate, :integer
-      
+
    end
 end
