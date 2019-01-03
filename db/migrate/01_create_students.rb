94c49c04652fd7da208cb79e3d8dde962f226b29
Priscilla H <priscillamhuynh@gmail.com>
2019-01-03 13:33:15 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,11 +1,8 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
 
   def change
-      create_table :name do |t|
+      create_table :students do |t|
        t.string :name
-       t.string :genre
-       t.integer :age
-       t.string :hometown
       end
    end
 end
